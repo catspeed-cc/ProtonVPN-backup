d291c532417e0a66b6914feea9be64a1424943c7
mooleshacat <admin@catspeed.cc>
2024-11-27 20:03:36 -0500

    added script_path

--- init.sh
+++ init.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/bash
 
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $0)
+
+# Absolute path this script is in. /home/user/bin
+export SCRIPT_PATH=`dirname $SCRIPT`
+
+cd ${SCRIPT_PATH}
+
 mkdir -p remote-repos
 mkdir -p backups
 
